e28823834e815e1e9c7ac81a2750beee9c236960
Felipe Reis <felipematheus17@hotmail.com>
2025-03-19 16:53:35 -0300

    fix: Next Auth URL callback

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,11 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: process.env.NEXTAUTH_URL + "/api/auth/callback/google",
+        },
+      },
     }),
   ],
   callbacks: {
